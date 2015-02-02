f7de5a275f37dd133cb314023fccf65b0961c05a
Tyler Elric <tyler9725@gmail.com>
2015-02-02 12:40:31 -0600

    service discovery extended. filesystem more concrete.

--- filesystem.py
+++ filesystem.py
@@ -6,9 +6,7 @@ class Directory:
 	def __init__(self,events,directory=None):
 		directory = directory or "."
 		self.directory = directory
-		self.events = events
-		events.listen(directory,self.handle_directory_request)
-	def handle_directory_request(self,addr,event):
+	def process(self,event):
 		if event['mode']=='read':
 			if event.get('format','data')=='raw':
 				self.read_raw(event)
@@ -21,17 +19,38 @@ class Directory:
 				self.write_raw(event)
 			else:
 				self.write_data(event)
+	def get_filename(self,event):
+		base_filename = event['filename']
+		base_directory = os.path.abspath(self.directory)
+		return os.path.join(base_directory,base_filename)
+	def open_file(self,event):
+		rw = 'r' if event.get('mode','read')=='read' else 'w'
+		return open(self.get_filename(event),rw)
 	def read_data(self,event):
-		pass
-	def write_data(self,event):
-		pass
-	def send_meta(self,event):
-		pass
-	def write_raw(self,event):
-		pass
+		with self.open_file(event) as f:
+			data = json.load(f)
 	def read_raw(self,event):
-		pass
+		with self.open_file(event) as f:
+			data = f.read()
+	def send_meta(self,event):
+		filename = self.get_filename(event)
+	def write_data(self,event):
+		with self.open_file(event) as f:
+			pass
+	def write_raw(self,event):
+		with self.open_file(event) as f:
+			pass
 
 class DirectoryService(Service):
 	def __init__(self,events):
 		super().__init__(events,'directory_service')
+		self.open_directories = {}
+	def respond(self,addr,event):
+		if event['directory'] not in self.open_directories:
+			self.open_directories = self.spawn_directory(event)
+		directory = self.open_directories[event['directory']]
+		result = directory.process(event)
+		if result:
+			self.emits.broadcast(result)
+	def spawn_directory(self,event):
+		return Directory(event['directory'])
--- service.py
+++ service.py
@@ -4,17 +4,31 @@ class Service:
 	def __init__(self,events,name,depends=None):
 		self.events = events
 		self.name = name
-		self.depends = depends or []
-		events.listen("service.discover",self.handle_service_event)
-		events.listen("service.starting",self.handle_service_event)
-		events.listen("service.ready",self.handle_service_event)
+		self.depends = set(depends or [])
+		self.discovered = set()
+		events.listen("services",self.handle_service_command)
 	def handle_service_event(self,addr,event):
-		if event['event']=="service.discover":
+		if event['discovery']=="discover":
 			self.acknowledge()
-		elif event['event'] == "service.starting":
+		elif event['discovery'] == "starting":
 			# the service has loaded,
 			# but is not ready for use yet.
 			pass
-		elif event['event'] == "service.ready":
+		elif event['discovery'] == "ready":
 			self.handle_service_ready(event['service'])
+	def initialize(self):
+		pass
+	def acknowledge(self):
+		pass
 	def handle_service_ready(self,service):
+		self.discovered.add(service)
+		if len(self.depends - self.discovered)<1:
+			# all dependancies started and ready.
+			self.initialize()
+	def handle_service_command(self,addr,event):
+		if event.get("service")==self.name:
+			self.respond(addr,event)
+		else:
+			self.handle_service_event(addr,event)
+	def respond(self,addr,event):
+		pass
